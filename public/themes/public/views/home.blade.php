73b518297e4412fa5b62f4b1c8f2680363879e1a
victor <orletchi.victor@gmail.com>
2016-04-30 21:47:20 +0300

    add design bue blok

--- public/themes/public/views/home.blade.php
+++ public/themes/public/views/home.blade.php
@@ -51,20 +51,75 @@
     </div> {{--end head block--}}
 
     <div class="container">
-        <div class="row">
+        <div class="row circles-holder">
             <div class="col-md-4 text-center">
                 <div class="circle-block">
                     <div class="circle-inner img-terms"></div>
+                </div>
+                <div class="title-circle">
+                    <h2>Сроки</h2>
+                    <h4>Сдача работы без задержек</h4>
                 </div>
             </div>
             <div class="col-md-4 text-center">
                 <div class="circle-block">
                     <div class="circle-inner img-quality"></div>
                 </div>
+                <div class="title-circle">
+                    <h2>Качество</h2>
+                    <h4>Соблюдение технологий</h4>
+                </div>
             </div>
             <div class="col-md-4 text-center">
                 <div class="circle-block">
                     <div class="circle-inner img-result"></div>
+                </div>
+                <div class="title-circle">
+                    <h2>Результат</h2>
+                    <h4>Удовлетворенность клиента</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row design-holder">
+            <div class="col-sm-12 title text-center">
+                <h1>Состав дизайн проекта</h1>
+            </div>
+        </div>
+        <div class="row design-holder-blue">
+            <div class="row-md-height">
+                <div class="col-xs-12 col-md-5 col-md-height col-middle">
+                    <div class="inside">
+                        <div class="content question-block text-center">
+                            <h1 class="bonuses">+ Бонус</h1>
+                            <p>2 выезда дизайнера во время работы!</p>
+                            <a href="" class="app-btn btn-orange">Задать вопрос</a>
+                        </div>
+                    </div>
+                </div><!--
+            --><div class="col-xs-12 col-md-7 col-md-height col-top">
+                    <div class="inside">
+                        <div class="content facility-block">
+                            <ul class="services-list">
+                                <li>Обмерный чертеж помещения</li>
+                                <li>План после перепланировки с экспликацией помещений</li>
+                                <li>План после перепланировки с размерами, уровнем пола и высотами</li>
+                                <li>План демонтажа перегородок</li>
+                                <li>Монтажный план перегородок</li>
+                                <li>План с расстоновкой мебели</li>
+                                <li>План пола с указанием типов покрытий</li>
+                                <li>План потолка</li>
+                                <li>Разрезы потолка с указанием высот</li>
+                                <li>План привязки приборов освещения</li>
+                                <li>План привязки приборов освещения и выключателей по группам</li>
+                                <li>План привязки элетротехнических изделий, совмещенный с мебелью</li>
+                                <li>План теплых полов</li>
+                                <li>Ведомость отделки по помещениям с указанием площадей, примеров, объемов</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
