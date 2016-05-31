d57775db70a0a11c2d981e2724739ef25e7f76cd
victor <orletchi.victor@gmail.com>
2016-06-01 00:40:23 +0300

    implement review and fixed slider buttons like design

--- public/themes/public/views/home.blade.php
+++ public/themes/public/views/home.blade.php
@@ -139,7 +139,6 @@
                     <li data-target="#myCarousel" data-slide-to="1"></li>
                     <li data-target="#myCarousel" data-slide-to="2"></li>
                 </ol>
-
                 <!-- Wrapper for Slides -->
                 <div class="carousel-inner">
                     <div class="item active">
@@ -159,10 +158,10 @@
 
                 <!-- Controls -->
                 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="icon-prev"></span>
+                    <span class="ionicons ion-ios-arrow-back"></span>
                 </a>
                 <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="icon-next"></span>
+                    <span class="ionicons ion-ios-arrow-forward"></span>
                 </a>
 
             </div><!-- end of carusel-->
@@ -227,6 +226,63 @@
                             <a class="app-btn btn-orange" href="">Прай-лист</a>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- end Projects slider block -->
+    <!-- Projects feedback block -->
+    <div class="container">
+        <div class="row reviews-holder">
+            <div class="col-sm-12 title text-center">
+                <h1 class="color-orange">Отзывы</h1>
+            </div>
+        </div>
+        <div class="row reviews-holder-slider">
+            <div id="myCarousel2" class="carousel slide" data-ride="carousel-">
+                <!-- Wrapper for Slides -->
+                <div class="carousel-inner">
+                    <div class="item container active">
+                        <!-- Set the first background image using inline CSS below. -->
+                        <div class="row circles-holder">
+                            <div class="col-sm-3 col-sm-offset-1 review-img-block">
+                                <div class="circle-block">
+                                    <div class="circle-inner img-review"></div>
+                                </div>
+                            </div>
+                            <div class="col-sm-7 review-content-block">
+                                <h1>Юля Студеникина</h1>
+                                <div class="quoted"><p>Для меня было истинным удовольствие работать с Екатериной Петровой. Она талантливый, креативный, яркий человек. Энергичная и жизнерадостная! Каждая ее работа не похожа на предыдущую, стили неповторимы. Она тонко чувствует Ваши потребности и предпочтения. Результат превосходит все ожидания!<br />Всем рекомендую!</p></div>
+                            </div>
+                        </div>
+                        {{--<div class="carousel-caption"><h2>Caption 1</h2></div>--}}
+                    </div>
+                    <div class="item container">
+                        <!-- Set the first background image using inline CSS below. -->
+                        <div class="row circles-holder">
+                            <div class="col-sm-3 col-sm-offset-1 review-img-block">
+                                <div class="circle-block">
+                                    <div class="circle-inner img-review"></div>
+                                </div>
+                            </div>
+                            <div class="col-sm-7 review-content-block">
+                                <h1>Юля Студеникина</h1>
+                                <div class="quoted"><p>Для меня было истинным удовольствие работать с Екатериной Петровой. Она талантливый, креативный, яркий человек. Энергичная и жизнерадостная! Каждая ее работа не похожа на предыдущую, стили неповторимы. Она тонко чувствует Ваши потребности и предпочтения. Результат превосходит все ожидания!<br />Всем рекомендую!</p></div>
+                            </div>
+                        </div>
+                        {{--<div class="carousel-caption"><h2>Caption 1</h2></div>--}}
+                    </div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#myCarousel2" data-slide="prev">
+                    <span class="ionicons ion-ios-arrow-back"></span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel2" data-slide="next">
+                    <span class="ionicons ion-ios-arrow-forward"></span>
+                </a>
+            </div><!-- end of carusel-->
+            <div class="row">
+                <div class="col-sm-12 text-right">
+                    <a href="" class="app-btn btn-orange">Отзывы клиентов</a>
                 </div>
             </div>
         </div>
